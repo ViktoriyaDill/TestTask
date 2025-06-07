6769e97e63570e1300d95d9482366cbafe37cca7
Viktoriia_Dill <Viktoriia_Dill>
2025-06-07 15:54:15 +0200

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Пользователь on 07.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
