d0a9c3e806d7342ca3b960c00724eca18c9ddfc7
Viktoriia_Dill <Viktoriia_Dill>
2025-06-14 21:24:12 +0200

    Added IQKeyboardManagerSwift for keyboard control

--- TestTask/TestTaskApp.swift
+++ TestTask/TestTaskApp.swift
@@ -6,12 +6,22 @@
 //
 
 import SwiftUI
+import IQKeyboardManagerSwift
 
 @main
 struct TestTaskApp: App {
+    
+    init() { configureKeyboard() }
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+        WindowGroup { ContentView() }
+    }
+    
+    private func configureKeyboard() {
+        let manager = IQKeyboardManager.shared
+
+        manager.isEnabled = true
+        manager.resignOnTouchOutside = true
+        manager.keyboardDistance = 10
     }
 }
